49d683f487ab35f7427e937a476072aa5e794181
artur <zieloneimperium230@o2.pl>
2025-03-26 19:28:11 +0100

    clean code

--- src/main/java/org/example/to/ExchangeForCurrency.java
+++ src/main/java/org/example/to/ExchangeForCurrency.java
@@ -1,12 +1,10 @@
 package org.example.to;
 
 import lombok.Data;
-import org.springframework.web.bind.annotation.ResponseStatus;
 
 import java.math.BigDecimal;
 
 @Data
-@ResponseStatus
 public class ExchangeForCurrency {
 
     private BigDecimal rate;
